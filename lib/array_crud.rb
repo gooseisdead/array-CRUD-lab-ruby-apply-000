75b4f22137d876930c7b20fc1a9bbac42276f3ed
Mike Smith <gooseisdead@mac.com>
2020-08-08 15:17:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["land", "island", "swamp", "forest"]
-  element = ["gate"]
-  array << element
+  array << "gate"
 end
 
 def add_element_to_start_of_array(array, element)
