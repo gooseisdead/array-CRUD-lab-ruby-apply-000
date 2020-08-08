daccb08163f4207e4e072d24748d4c374b62fced
Mike Smith <gooseisdead@mac.com>
2020-08-08 15:25:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,12 +11,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["land", "island", "swamp", "forest"]
   array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["land", "island", "swamp", "forest", arrays]
+  ["land", "island", "swamp", "forest", "arrays!"]
   array.pop
 end
 
